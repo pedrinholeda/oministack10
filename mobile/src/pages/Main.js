b064c5294ae0ad6f6d826fc63377917160d7f719
Pedro Leda <pedro.henrique.leda@gmail.com>
2020-01-17 12:31:42 -0300

    feita o link com o mapa, inserindo mark com foto de usuario;

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Image } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import {
+  requestPermissionsAsync,
+  getCurrentPositionAsync
+} from "expo-location";
+
+function Main() {
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true
+        });
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          //calculando zoom do mapa
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04
+        });
+      }
+    }
+    loadInitialPosition();
+  }, []);
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: 37.7822325, longitude: -122.4129692 }}>
+        <Image
+          style={styles.avatar}
+          source={{
+            uri: "https://avatars3.githubusercontent.com/u/40272724?v=4"
+          }}
+        />
+      </Marker>
+    </MapView>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: "#fff"
+  }
+});
+
+export default Main;
